e6b6f60424da203a91d668dd2eda1ccf2762cb51
mcldgcode <121442093+mcldgcode@users.noreply.github.com>
2023-08-15 13:18:37 +0200

    Update page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           height="48"
           width="48"
           className="mx-auto w-auto"
-          src="https://i.ibb.co/9ZQyTkv/Skilledgram-logo-no-bg.png"
+          src="https://s3.amazonaws.com/my-bucket/my-image.png"
           href="/"
         />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200">
